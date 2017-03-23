d17f6e64b51413c3dfd9b8820db36cc8dd884bfd
chenshancang <chenshancang@jucaicat.com>
2017-03-23 14:10:47 +0800

    init

---
+++ gotdate.py
@@ -0,0 +1,84 @@
+# -*- coding: UTF-8 -*-
+__author__ = "chenshancang@163.com"
+
+import datetime
+
+
+class GotoDate(object):
+	def __init__(self,date):
+		self.date = date
+
+	def is_valid_date(self):
+		#判断日期有效性
+		if isinstance(self.date,basestring):
+			try:
+				datetime.datetime.strptime(self.date,'%Y-%m-%d')
+			except Exception:
+				return False
+			return True
+		return True
+
+	def _clean_date(self):
+		#字符串转行成日期类型
+		if self.is_valid_date():
+			if isinstance(self.date,basestring):
+				return datetime.datetime.strptime(self.date,'%Y-%m-%d').date()
+			return self.date
+
+	def before(self,num):
+		#输出几天前
+		days = datetime.timedelta(days=num)
+		return self._clean_date() - days
+
+
+	def after(self,num):
+		#输出几天后
+		days = datetime.timedelta(days=num)
+		return self._clean_date() + days
+
+	def week(self):
+		#返回周几
+		return self._clean_date().weekday()
+
+	def week_range(self,china=False):
+		#返回本周的所有日期
+		if china == True:
+			index = 2
+			#输出中文
+		else:
+			#输出英文
+			index =0
+		week = (	('Monday',		0	,u'周一'),
+					('Tuesday',		1	,u'周二'),
+					('Wednesday',	2	,u'周三'),
+					('Thursday',	3	,u'周四'),
+					('Friday',		4	,u'周五'),
+					('Saturday',	5	,u'周六'),
+					('Sunday',		6	,u'周日')
+				)
+		weekday_range={}
+		for weekday in week:
+			weekday_range[weekday[index]] = self.before(self.week() - weekday[1])
+			if weekday[1] == self.week():
+				weekday_range[weekday[index]] = self.after(weekday[1] - self.week())
+
+		return weekday_range
+
+
+
+
+
+
+
+
+
+a='2015-06-11'
+f=GotoDate(datetime.datetime.now().date())
+#f=GotoDate(a)
+if f.is_valid_date():
+	print f.before(3)
+	print f.after(5)
+	print f.week()
+	print f.week_range(china=True)
+	for k,v in f.week_range(china=True).items():
+		print k,v
